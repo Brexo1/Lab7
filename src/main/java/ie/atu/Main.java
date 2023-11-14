10325e60f50edbb1a07bf78fea39b302a63984bf
Brendan <G00404587@atu.ie>
2023-11-14 14:01:21 +0000

    Now allows user to enter new number even after entering invalid character

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println("Please enter a number between 0 - 9: ");
-            Scanner myscan = new Scanner(System.in);
+        while (true) {
 
-            //read user input as a string
-            String input = myscan.nextLine();
+            try {
+                System.out.println("Please enter a number between 0 - 9: ");
+                Scanner myscan = new Scanner(System.in);
 
-            //pass the input to an integer
-            int myNum = Integer.parseInt(input);
+                //read user input as a string
+                String input = myscan.nextLine();
 
-            //check if the number entered is between 0 and 9
-            if (myNum >= 0 && myNum <= 9) {
-                System.out.println("You entered " + myNum);
-            } else {
+                //pass the input to an integer
+                int myNum = Integer.parseInt(input);
 
-                System.out.println("Not a valid Number");
+                //check if the number entered is between 0 and 9
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("You entered " + myNum);
+                } else {
+
+                    System.out.println("Not a valid Number");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a valid numeric value: ");
             }
-        } catch (NumberFormatException e) {
-            System.out.println("Enter a valid numeric value: ");
         }
     }
 }
