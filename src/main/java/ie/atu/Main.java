b3f466e34eb36c044645e9a87a1f10bcf904069b
Brendan <G00404587@atu.ie>
2023-11-14 13:28:21 +0000

    Created lab 7 and added code provided

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,24 @@
+package ie.atu;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter a number between 0 - 9: ");
+        Scanner myscan = new Scanner(System.in);
+
+        //read user input as a string
+        String input = myscan.nextLine();
+
+        //pass the input to an integer
+        int myNum = Integer.parseInt(input);
+
+        //check if the number entered is between 0 and 9
+        if (myNum >= 0 && myNum <= 9){
+            System.out.println("You entered " + myNum);
+        }
+        else{
+            System.out.println("Not a valid Number");
+        }
+    }
+}
